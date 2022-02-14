53b774d84ba7529ad2c9a5bb9c7d01e67af44487
Yuqiu Ge <yuqiu.ge@gmail.com>
2022-02-14 14:03:27 +0100

    works now

--- cicdFrontend/src/app/components/greeting/greeting.component.ts
+++ cicdFrontend/src/app/components/greeting/greeting.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { GreetingService } from '../../services/greeting.service';
+import { Greeting } from '../../models/greeting.model';
 
 @Component({
   selector: 'app-greeting',
@@ -9,11 +10,15 @@ import { GreetingService } from '../../services/greeting.service';
 export class GreetingComponent implements OnInit {
 
   @Input()
-  greeting: string = 'Hello, Peter!'
+  greeting: Greeting = {
+    content: 'Hello, Peter!'
+  } as Greeting;
 
   constructor(private readonly greetingService: GreetingService) {}
 
   ngOnInit(): void {
-    this.greetingService.fetch().subscribe(greeting => this.greeting = greeting);
+    this.greetingService.fetch().subscribe(greeting => {
+      this.greeting = greeting
+    });
   }
 }
--- cicdFrontend/src/app/services/greeting.service.ts
+++ cicdFrontend/src/app/services/greeting.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { Greeting } from '../models/greeting.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,8 @@ export class GreetingService {
 
   constructor(private readonly httpClient: HttpClient) { }
 
-  fetch(): Observable<string> {
+  fetch(): Observable<Greeting> {
     // @ts-ignore
-    return this.httpClient.get(environment.api.greeting).pipe(map((res: EntityArrayResponseType) => res ? res.body : ''));
+    return this.httpClient.get(environment.api.greeting).pipe(map((res: Greeting) => res));
   }
 }
-
-type EntityArrayResponseType = HttpResponse<string>;
